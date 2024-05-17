695b90829f9e4e6a17b2207bdc8200a06c464928
Omar Farghaly <101020003+omar00050@users.noreply.github.com>
2024-05-17 15:41:33 +0300

    Added New Channels Radio

--- src/utils/functions/RadioChannels.js
+++ src/utils/functions/RadioChannels.js
@@ -1,58 +1,104 @@
 module.exports = [{
   name: "إذاعة القران الكريم دولة مصر",
   value: "http://n02.radiojar.com/v33ay8543d0uv?rj-ttl=5&rj-tok=AAABgDTqH90AIyBNaL5t4qE1IA"
-}, {
+},
+{
   name: "إذاعة القران الكريم دولة السعودية",
   value: "http://n0e.radiojar.com/4wqre23fytzuv?rj-ttl=5&rj-tok=AAABgDTrkhEAsmBCfbT280gMag"
 }, /*{
   name: "إذاعة القران الكريم دولة البحرين",
   value: "https://prod-34-82-94-205.wostreaming.net/ihorizonsradio-sakfmmp3-ibc1?session-id=3453c3202479222be48941d99f762a88"
-},*/ {
-  name: "إذاعة أحمد الطرابلسي",
-  value: "https://Qurango.net/radio/ahmed_altrabulsi"
-}, {
-  name: "إذاعة أحمد خضر الطرابلسي",
-  value: "https://Qurango.net/radio/ahmad_khader_altarabulsi"
-}, {
-  name: "إذاعة ابراهيم الدوسري",
-  value: "https://Qurango.net/radio/ibrahim_aldosari"
-}, {
+},*/
+
+{
   name: "إذاعة ماهر المعيقلي",
   value: "https://Qurango.net/radio/maher_al_meaqli"
-}, {
+},
+{
+  name: " إذاعة محمد صديق المنشاوي",
+  value: "https://backup.Qurango.net/radio/mohammed_siddiq_alminshawi"
+},
+{
+  name: "إذاعة محمد صديق المنشاوي المصحف المجود",
+  value: "https://backup.Qurango.net/radio/mohammed_siddiq_alminshawi_mojawwad"
+},
+{
   name: "إذاعة عبدالباسط عبدالصمد",
   value: "https://Qurango.net/radio/abdulbasit_abdulsamad_warsh"
-}, {
-  name: "إذاعة تفسير القران الكريم",
-  value: "https://qurango.net/radio/tafseer"
-}, {
-  name: "أذكار الصباح",
-  value: "https://qurango.net/radio/athkar_sabah"
-}, {
-  name: "أذكار المساء",
-  value: "https://qurango.net/radio/athkar_masa"
-}, {
-  name: "إذاعة محمد عبدالكريم",
-  value: "https://Qurango.net/radio/mohammad_abdullkarem"
-}, {
+},
+{
+  name: "إذاعة عبدالباسط عبدالصمدالمصحف المجود",
+  value: "https://Qurango.net/radio/abdulbasit_abdulsamad_mojawwad"
+},
+{
   name: "إذاعة محمود علي البنا",
   value: "https://Qurango.net/radio/mahmoud_ali__albanna"
-}, {
+},
+{
+  name: "إذاعة محمود علي البنا المصحف المجود",
+  value: "https://Qurango.net/radio/mahmoud_ali__albanna_mojawwad"
+},
+{
+  name: "إذاعة خالد الجليل",
+  value: "https://Qurango.net/radio/khalid_aljileel"
+},
+{
+  name: "إذاعة عبدالله الخلف",
+  value: "https://Qurango.net/radio/abdullah_alkhalaf"
+},
+{
+  name: "إذاعة أحمد الطرابلسي",
+  value: "https://Qurango.net/radio/ahmed_altrabulsi"
+},
+{
+  name: " إذاعة مشاري العفاسي",
+  value: "https://Qurango.net/radio/mishary_alafasi"
+},
+{
+  name: "إذاعة ابراهيم الدوسري",
+  value: "https://Qurango.net/radio/ibrahim_aldosari"
+},
+{
+  name: "إذاعة محمد عبدالكريم",
+  value: "https://Qurango.net/radio/mohammad_abdullkarem"
+},
+{
   name: "إذاعة محمود خليل الحصري",
   value: "https://Qurango.net/radio/mahmoud_khalil_alhussary"
-}, {
+},
+{
+  name: " إذاعة فارس عباد",
+  value: "https://Qurango.net/radio/fares_abbad"
+},
+{
   name: "إذاعة علي الحذيفي",
   value: "https://Qurango.net/radio/ali_alhuthaifi_qalon"
-}, {
+},
+{
+  name: "إذاعة تفسير القران الكريم",
+  value: "https://qurango.net/radio/tafseer"
+},
+{
   name: "السيرة النبوية - 400 حلقة عن سيرة نبينا محمد",
   value: "https://Qurango.net/radio/fi_zilal_alsiyra"
-}, {
+},
+{
+  name: "أذكار الصباح",
+  value: "https://qurango.net/radio/athkar_sabah"
+},
+{
+  name: "أذكار المساء",
+  value: "https://qurango.net/radio/athkar_masa"
+},
+{
   name: "إذاعة عبدالرحمن السديس",
   value: "https://Qurango.net/radio/abdulrahman_alsudaes"
-}, {
+},
+{
   name: "إذاعة أحمد العجمي",
   value: "https://Qurango.net/radio/ahmad_alajmy"
-}, {
+},
+{
   name: "إذاعة سعد الغامدي",
   value: "https://Qurango.net/radio/saad_alghamdi"
 }]
