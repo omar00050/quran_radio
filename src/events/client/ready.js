1c99dc660c3cc6adedc53101b41b0e9ca744fbfd
Omar Farghaly <101020003+omar00050@users.noreply.github.com>
2024-05-31 08:54:22 +0300

    added Activity bot status running

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -1,5 +1,6 @@
 const joinAndPlayQuran = require('@root/src/utils/functions/joinAndPlayQuran');
 const chalk = require('chalk');
+const { ActivityType, } = require('discord.js');
 const gr = chalk.hex('#00D100');
 const un = chalk.underline;
 
@@ -15,25 +16,31 @@ module.exports = {
       console.log(gr(`Logged In As ` + un(`${client.user.username}`)));
       console.log(chalk.cyan(`Servers:` + un(`${client.guilds.cache.size}`)), chalk.red(`Users:` + un(`${client.users.cache.size}`)), chalk.blue(`Commands:` + un(` ${client.commands.size}` + ` TOTAL Commands ${client.commands.size + commands.length}`)));
     }, 500);
-
+    client.user.setStatus("idle")
+    client.user.setActivity({ name: `Loading....`, type: ActivityType.Playing })
+    setTimeout(() => client.user.setStatus("online"), 40000);
+    setInterval(() => {
+      let ServersStatus = client.Radio.size
+      client.user.setActivity({ name: `in ${ServersStatus} Server`, type: ActivityType.Listening })
+    }, 1 * 1000 * 60);
 
     let RadioChannels = await client.db.table("channels").values() || [];
     if (RadioChannels.length === 0) return
-      setTimeout(async () => {
+    setTimeout(async () => {
 
-        for (let data of RadioChannels) {
-          if (data.enabled) {
+      for (let data of RadioChannels) {
+        if (data.enabled) {
 
-            let guild = await client.guilds.fetch(data.guildId)
-            if (!guild) continue
-            let conn = await joinAndPlayQuran(client, data.channelId, guild, data.url)
-            if (conn === null) continue
-            if (conn === "cantConnect") continue
-            client.Radio.set(data.guildId, conn)
-          }
-
+          let guild = await client.guilds.fetch(data.guildId)
+          if (!guild) continue
+          let conn = await joinAndPlayQuran(client, data.channelId, guild, data.url)
+          if (conn === null) continue
+          if (conn === "cantConnect") continue
+          client.Radio.set(data.guildId, conn)
         }
-      }, 1000);
+
+      }
+    }, 1000);
 
   },
 };
