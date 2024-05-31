e019edaa849c2f833d6054d306dc36c5c535f44f
Omar Farghaly <101020003+omar00050@users.noreply.github.com>
2024-05-31 08:28:56 +0300

    added new radio yasser_aldosari

--- src/utils/functions/RadioChannels.js
+++ src/utils/functions/RadioChannels.js
@@ -5,11 +5,18 @@ module.exports = [{
 {
   name: "إذاعة القران الكريم دولة السعودية",
   value: "http://n0e.radiojar.com/4wqre23fytzuv?rj-ttl=5&rj-tok=AAABgDTrkhEAsmBCfbT280gMag"
-}, /*{
+},
+/* 
+{
   name: "إذاعة القران الكريم دولة البحرين",
   value: "https://prod-34-82-94-205.wostreaming.net/ihorizonsradio-sakfmmp3-ibc1?session-id=3453c3202479222be48941d99f762a88"
-},*/
+},
+*/
 
+{
+  name: "إذاعة ياسر الدوسري",
+  value: "https://Qurango.net/radio/yasser_aldosari"
+},
 {
   name: "إذاعة ماهر المعيقلي",
   value: "https://Qurango.net/radio/maher_al_meaqli"
